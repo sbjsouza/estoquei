4c2717270c35b5433bca8b6c48482a0742cf202b
Sergio <sslp@cin.ufpe.br>
2021-04-22 07:56:46 -0300

    adicionando rota de vendas

--- estoquei-project/src/app/app-routing.module.ts
+++ estoquei-project/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VendasComponent } from './vendas/vendas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'vendas-component', component: VendasComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- estoquei-project/src/app/app.module.ts
+++ estoquei-project/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { VendasComponent } from './vendas/vendas.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent],
+  declarations: [AppComponent, CardComponent, VendasComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
