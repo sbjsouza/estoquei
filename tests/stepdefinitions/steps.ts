a4ec9089e0e7e85328661f6aa8e882ead1f575f4
Samuel Souza <sbjs@cin.ufpe.br>
2021-04-29 16:33:08 -0300

    Fix dead code refactoring

--- tests/stepdefinitions/steps.ts
+++ tests/stepdefinitions/steps.ts
@@ -6,52 +6,6 @@ import request = require("request-promise");
 
 import { Vendedor } from "../../estoquei-project/src/app/models/vendedor";
 
-var base_url = "http://localhost:3000/";
-
-let sameCPF = (elem, cpf) =>
-  elem
-    .element(by.name("cpflist"))
-    .getText()
-    .then((text) => text === cpf);
-let sameName = (elem, name) =>
-  elem
-    .element(by.name("nomelist"))
-    .getText()
-    .then((text) => text === name);
-
-let pAND = (p, q) => p.then((a) => q.then((b) => a && b));
-
-// async function criarAluno(name, cpf) {
-//   await $("input[name='namebox']").sendKeys(<string>name);
-//   await $("input[name='cpfbox']").sendKeys(<string>cpf);
-//   await element(by.buttonText("Adicionar")).click();
-// }
-
-// async function assertTamanhoEqual(set, n) {
-//   await set.then((elems) =>
-//     expect(Promise.resolve(elems.length)).to.eventually.equal(n)
-//   );
-// }
-
-// async function assertElementsWithSameCPFAndName(n, cpf, name) {
-//   var allalunos: ElementArrayFinder = element.all(by.name("alunolist"));
-//   var samecpfsandname = allalunos.filter((elem) =>
-//     pAND(sameCPF(elem, cpf), sameName(elem, name))
-//   );
-//   await assertTamanhoEqual(samecpfsandname, n);
-// }
-
-// async function assertElementsWithSameCPF(n, cpf) {
-//   var allalunos: ElementArrayFinder = element.all(by.name("alunolist"));
-//   var samecpfs = allalunos.filter((elem) => sameCPF(elem, cpf));
-//   await assertTamanhoEqual(samecpfs, n);
-// }
-
-async function getScore(vendedor: Vendedor) {
-  return vendedor.monthly_sells != 0
-    ? parseInt(vendedor.monthly_sales_price) / vendedor.monthly_sells
-    : 0;
-}
 async function getSellerId(id: String) {
   const options = {
     method: `GET`,
@@ -112,8 +66,6 @@ defineSupportCode(function ({ Given, When, Then }) {
     await expect(element(by.css("#pagename")).getText()).to.eventually.equal(
       pagename.toString()
     );
-    // await expect(browser.getTitle()).to.eventually.equal("TaGui");
-    // await $("a[name='alunos']").click();
   });
   Given(
     /^o vendedor está registrado com o id "(\d*)", nome "([^\"]*)", Número de vendas "(\d*)" e Valor bruto de vendas "([^\"]*)"$/,
@@ -135,15 +87,9 @@ defineSupportCode(function ({ Given, When, Then }) {
       pagename.toString()
     );
   });
-
   When(/^eu pergunto ao sistema pelo vendedor com id "(\d*)"$/, async (id) => {
     seller = await getSellerId(id.toString());
-    // score = (await getScore(seller)).toString();
   });
-
-  // Then(/^o sistema retorna o vendedor que está registrado com o id "1", nome "Rafael Portugal", Número de vendas "1" e Valor bruto de vendas "32000.00""(\d*)"$/, async (localscore) => {
-  //   await expect(score).to.equal(localscore.toString());
-  // });
   Then(
     /^o sistema retorna o vendedor que está registrado com o id "(\d*)", nome "([^\"]*)", Número de vendas "(\d*)" e Valor bruto de vendas "([^\"]*)"$/,
     async (id, name, monthly_sells, monthly_sales_price) => {
@@ -175,89 +121,6 @@ defineSupportCode(function ({ Given, When, Then }) {
           });
           expect(validate).to.equal(true);
         });
-
-      // res.forEach((seller, index) =>
-      //   expect(seller[0].getText()).to.equal(tmp_list[index].toString())
-      // )
-
-      // await expect().to.eventually.equal(
-      //   pagename.toString()
-      // );
     }
   );
-
-  //   Given(
-  //     /^I cannot see a student with CPF "(\d*)" in the students list$/,
-  //     async (cpf) => {
-  //       await assertElementsWithSameCPF(0, cpf);
-  //     }
-  //   );
-
-  //   When(
-  //     /^I try to register the student "([^\"]*)" with CPF "(\d*)"$/,
-  //     async (name, cpf) => {
-  //       await criarAluno(name, cpf);
-  //     }
-  //   );
-
-  //   Then(
-  //     /^I can see "([^\"]*)" with CPF "(\d*)" in the students list$/,
-  //     async (name, cpf) => {
-  //       await assertElementsWithSameCPFAndName(1, cpf, name);
-  //     }
-  //   );
-
-  //   Given(
-  //     /^I can see a student with CPF "(\d*)" in the students list$/,
-  //     async (cpf) => {
-  //       await criarAluno("Clarissa", cpf);
-  //       await assertElementsWithSameCPF(1, cpf);
-  //     }
-  //   );
-
-  //   Then(
-  //     /^I cannot see "([^\"]*)" with CPF "(\d*)" in the students list$/,
-  //     async (name, cpf) => {
-  //       await assertElementsWithSameCPFAndName(0, cpf, name);
-  //     }
-  //   );
-
-  //   Then(/^I can see an error message$/, async () => {
-  //     var allmsgs: ElementArrayFinder = element.all(by.name("msgcpfexistente"));
-  //     await assertTamanhoEqual(allmsgs, 1);
-  //   });
-
-  //   Given(/^the system has no student with CPF "(\d*)"$/, async (cpf) => {
-  //     await request
-  //       .get(base_url + "alunos")
-  //       .then((body) => expect(body.includes(`"cpf":"${cpf}"`)).to.equal(false));
-  //   });
-
-  //   When(
-  //     /^I register the student "([^\"]*)" with CPF "(\d*)"$/,
-  //     async (name, cpf) => {
-  //       let aluno = { nome: name, cpf: cpf, email: "" };
-  //       var options: any = {
-  //         method: "POST",
-  //         uri: base_url + "aluno",
-  //         body: aluno,
-  //         json: true,
-  //       };
-  //       await request(options).then((body) =>
-  //         expect(JSON.stringify(body)).to.equal(
-  //           '{"success":"O aluno foi cadastrado com sucesso"}'
-  //         )
-  //       );
-  //     }
-  //   );
-
-  //   Then(
-  //     /^the system now stores "([^\"]*)" with CPF "(\d*)"$/,
-  //     async (name, cpf) => {
-  //       let resposta = `{"nome":"${name}","cpf":"${cpf}","email":"","metas":{}`;
-  //       await request
-  //         .get(base_url + "alunos")
-  //         .then((body) => expect(body.includes(resposta)).to.equal(true));
-  //     }
-  //   );
 });
