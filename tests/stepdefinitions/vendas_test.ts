54bd1f7b7bfd78d99ff25b1cf1a5da9824ec61ad
Sergio <sslp@cin.ufpe.br>
2021-04-29 14:24:35 -0300

    test de service

--- tests/stepdefinitions/vendas_test.ts
+++ tests/stepdefinitions/vendas_test.ts
@@ -3,9 +3,54 @@ import { browser, $, element, ElementArrayFinder, by } from 'protractor';
 import * as fs from 'fs';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
+import request = require("request-promise");
 
-
+import { Vendas } from "../../estoquei-project/src/app/models/vendas";
+import { parse } from 'querystring';
 let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
+
+let total_value = "";
+
+async function getTotal(venda: Vendas) {
+  return parseFloat(venda.value)*venda.quantity
+}
+
+async function getSaleID(id: String) {
+  const options = {
+    method: `GET`,
+    json: true,
+    uri: `http://localhost:3000/vendas/${id}`,
+  };
+  try {
+    const response = await request(options);
+    return response;
+  } catch (error) {
+    Promise.reject(error);
+    expect(false).to.equal(true);
+  }
+}
+
+async function checkProd(
+  id: String,
+  product: String,
+) {
+  const options = {
+    method: `GET`,
+    json: true,
+    uri: `http://localhost:3000/vendas/${id}`,
+  };
+  try {
+    const response = await request(options);
+    return Promise.resolve(
+      response.id.toString() == id.toString() 
+        ? expect(response.product.toString()).to.equal(product)
+        : expect(false).to.equal(true)
+    );
+  } catch (error) {
+    Promise.reject(error);
+    expect(false).to.equal(true);
+  }
+}
 
 defineSupportCode(function ({ Given, When, Then }) {
     Given(/^eu estou na página de "([^\"]*)"$/, async (pagename)=> {
@@ -15,14 +60,37 @@ defineSupportCode(function ({ Given, When, Then }) {
         );
     })
 
-    Given(/^eu clico em "([^\"]*)"$/, async (button) => {
+    Given(
+        /^uma venda cadastrada no sistema do produto "([^\"]*)" que tem id "([^\"]*)"$/,
+        async (product, id) => {
+          await browser.get("http://localhost:4200/");
+          await checkProd(
+            id.toString(),
+            product.toString()
+          );
+        }
+      );
+
+    When(/^eu clico em "([^\"]*)"$/, async (button) => {
         await element(by.buttonText(<string> button)).click();
     });
+
+    When(
+        /^eu pergunto ao sistema sobre o total ganho na venda de id "(\d*)"$/,
+        async (id) => {
+          let sale = await getSaleID(id.toString());
+          total_value = (await getTotal(sale)).toString();
+        }
+      );
 
 
     Then(/^o sistema baixa o arquivo "([^\"]*)"$/, async (file_name) => {
         
         await fs.existsSync(<string>file_name);
     });
+
+    Then(/^o sistema retorna o total de "([^\"]*)"$/, async (value) => {
+        await expect(total_value).to.equal(value);
+      });
 
 })
