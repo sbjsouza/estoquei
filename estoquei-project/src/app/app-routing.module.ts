19deae2c0ca2d2b3190d13fbe02c886ab75ae4a8
Marco Santana <marco@capyba.com>
2021-04-30 17:05:27 -0300

    Add dashboard component

--- estoquei-project/src/app/app-routing.module.ts
+++ estoquei-project/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { VendasComponent } from './vendas/vendas.component';
 import { ProdutosComponent } from './produtos/produtos.component';
 import { VendedorComponent } from './vendedor/vendedor.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'vendas', component: VendasComponent },
   { path: '', component: VendasComponent },
   { path: 'produtos', component: ProdutosComponent },
   { path: 'vendedor', component: VendedorComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
