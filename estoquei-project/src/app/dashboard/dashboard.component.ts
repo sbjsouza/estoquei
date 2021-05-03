3123751bc72862424391aff7f64959360b4625b2
Marco Santana <marco@capyba.com>
2021-05-03 12:00:41 -0300

    Add dashboard page component

--- estoquei-project/src/app/dashboard/dashboard.component.ts
+++ estoquei-project/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Vendas } from '../models/vendas';
+import { VendasService } from '../services/vendas.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  topRevenues: Observable<Vendas[]>;
+  topSales: Observable<Vendas[]>;
+  vendas: Observable<Vendas[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private vendasService: VendasService) {
+    console.log('heree');
   }
 
+  ngOnInit(): void {
+    this.vendas = this.vendasService.getSells();
+    this.topRevenues = this.vendas.pipe(
+      map(sales=>{
+        sales = sales.map(sale=>{
+          sale['revenue'] = sale.quantity * parseFloat(sale.value);
+          return sale;
+        }).sort((a, b) => b['revenue'] - a['revenue']).filter((_, i)=> i<3);
+        return sales;
+    }));
+    this.topSales = this.vendas.pipe(
+      map(sales=>{
+        sales = sales.sort((a, b) => b.quantity - a.quantity).filter((_, i)=> i<3);
+        return sales;
+    }));
+  }
 }
