efdebe6a16e8e82f0b5e6283fa87dd0c95b54934
Hugo Santos <hss@cin.ufpe.br>
2021-05-02 21:16:35 -0300

    store's images list addition

--- estoquei-project/src/app/models/lojas.ts
+++ estoquei-project/src/app/models/lojas.ts
@@ -1,5 +1,6 @@
 export class Lojas {
     name: string;
+    image: string;
     store_localization_url: string;
     id: number;
     monthly_sells: number;
@@ -12,7 +13,7 @@ export class Lojas {
   
     clean(): void {
       this.name = '';
-      this.store_localization_url = '';
+      this.image = '';      this.store_localization_url = '';
       this.id = 0;
       this.monthly_sells = 0;
       this.monthly_expense_cost = 0;
